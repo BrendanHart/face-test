c1166edbebcf3ca9beedf1b1d8a4f2d26f793872
Brendan Hart <brendanrayhart@gmail.com>
2017-12-14 00:19:25 +0000

    Varying scale for HOG

--- perform.py
+++ perform.py
@@ -44,17 +44,13 @@ for scale_val in scale_values:
         l_true_positives = 0
         l_false_positives = 0
 
-        g_true_negatives = 0
-        g_false_negatives = 0
-        g_true_positives = 0
-        g_false_positives = 0
 
         positives_used = 0
         negatives_used = 0
 
         for path in faces:
             
-            print "SCALE " + str(scale_val) + " NEIGHBOURS " + str(neighbour_val) + ": " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
+            #print "SCALE " + str(scale_val) + " NEIGHBOURS " + str(neighbour_val) + ": " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
 
             if positives_used >= NUM_POSITIVES:
                 break
@@ -73,20 +69,12 @@ for scale_val in scale_values:
             else:
                 l_false_negatives += 1
 
-            (rects, weights) = hog.detectMultiScale(gray,
-                                winStride=(4,4),
-                                padding=(8,8),
-                                scale=1.05)
-            if len(rects) > 0:
-                g_true_positives += 1
-            else:
-                g_false_negatives += 1
 
             positives_used += 1
 
         for path in nofaces:
 
-            print "SCALE " + str(scale_val) + " NEIGHBOURS " + str(neighbour_val) + ": " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
+            #print "SCALE " + str(scale_val) + " NEIGHBOURS " + str(neighbour_val) + ": " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
 
             if negatives_used >= NUM_NEGATIVES:
                 break
@@ -104,24 +92,71 @@ for scale_val in scale_values:
             else:
                 l_true_negatives += 1
 
-            (rects, weights) = hog.detectMultiScale(gray,
-                                winStride=(4,4),
-                                padding=(8,8),
-                                scale=1.05)
-            if len(rects) > 0:
-                g_false_positives += 1
-            else:
-                g_true_negatives += 1
-
             negatives_used += 1
 
         h_precision = (float(h_true_positives)/float(h_true_positives+h_false_positives))
         h_recall = (float(h_true_positives)/float(h_true_positives+h_false_negatives))
         l_precision = (float(l_true_positives)/float(l_true_positives+l_false_positives))
         l_recall = (float(l_true_positives)/float(l_true_positives+l_false_negatives))
-        g_precision = (float(g_true_positives)/float(g_true_positives+g_false_positives))
-        g_recall = (float(g_true_positives)/float(g_true_positives+g_false_negatives))
 
+        print "SCALE: " + str(scale_val) + " NEIGHBOURS: " + str(neighbour_val)
         print "HAAR RESULT: " + str( float(2 * h_precision * h_recall) / float(h_precision + h_recall))
         print "LBP RESULT: " + str( float(2 * l_precision * l_recall) / float(l_precision + l_recall))
-        print "HOG RESULT: " + str( float(2 * g_precision * g_recall) / float(g_precision + g_recall))
+
+for scale in scale_values:
+
+    g_true_negatives = 0
+    g_false_negatives = 0
+    g_true_positives = 0
+    g_false_positives = 0
+
+    positives_used = 0
+    negatives_used = 0
+
+    for path in faces:
+        
+        print "SCALE " + str(scale) + " " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
+
+        if positives_used >= NUM_POSITIVES:
+            break
+
+        img = cv2.imread(path)
+        img = imutils.resize(img, width=min(400, img.shape[1]))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        (rects, weights) = hog.detectMultiScale(gray,
+                            winStride=(4,4),
+                            padding=(8,8),
+                            scale=scale)
+        if len(rects) > 0:
+            g_true_positives += 1
+        else:
+            g_false_negatives += 1
+
+        positives_used += 1
+
+    for path in nofaces:
+
+        #print "SCALE " + str(scale) + " " + str(100*(positives_used+negatives_used)/float(NUM_POSITIVES+NUM_NEGATIVES))+"%"
+
+        if negatives_used >= NUM_NEGATIVES:
+            break
+        
+        img = cv2.imread(path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        (rects, weights) = hog.detectMultiScale(gray,
+                            winStride=(4,4),
+                            padding=(8,8),
+                            scale=scale)
+        if len(rects) > 0:
+            g_false_positives += 1
+        else:
+            g_true_negatives += 1
+
+        negatives_used += 1
+
+    g_precision = (float(g_true_positives)/float(g_true_positives+g_false_positives))
+    g_recall = (float(g_true_positives)/float(g_true_positives+g_false_negatives))
+
+    print "SCALE " + str(scale) + " HOG RESULT: " + str( float(2 * g_precision * g_recall) / float(g_precision + g_recall))
--- times.py
+++ times.py
@@ -5,6 +5,7 @@ import os
 import time
 
 scale_values = [1.01, 1.1, 1.3, 1.5]
+hog_scales = [(4,4), (8,8), (16,16)]
 
 def get_files(root_paths):
     paths = []
@@ -28,7 +29,6 @@ if len(faces) < 5:
 for scale_val in scale_values:
     h_time = 0.0
     l_time = 0.0
-    g_time = 0.0
     for i in range(5):
         img = cv2.imread(faces[i])
         img = imutils.resize(img, width=min(400, img.shape[1]))
@@ -42,11 +42,20 @@ for scale_val in scale_values:
         lbp_cascade.detectMultiScale(gray, scale_val, 3)
         l_time += (time.time() - start)
 
+
+    print "SCALE " + str(scale_val) + " HAAR-" + str(h_time/5.0) + " LBP-" + str(l_time/5.0)
+
+for scale in scale_values:
+    g_time = 0.0
+    for i in range(5):
+        img = cv2.imread(faces[i])
+        img = imutils.resize(img, width=min(400, img.shape[1]))
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
         start = time.time()
         (rects, weights) = hog.detectMultiScale(gray,
                             winStride=(4,4),
                             padding=(8,8),
-                            scale=1.05)
+                            scale=scale)
         g_time += (time.time() - start)
-
-    print "SCALE " + str(scale_val) + " HAAR-" + str(h_time/5.0) + " LBP-" + str(l_time/5.0) + " HOG-" + str(g_time/5.0)
+    print "SCALE " + str(scale) + " HOG-" + str(g_time/5.0)
